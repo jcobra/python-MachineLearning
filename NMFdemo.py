58be60107c076b0115b0af980dcd3d8e75c5c499
jingzl <jingzl@hotmail.com>
2020-05-07 11:02:48 +0800

    NMF demo

---
+++ NMFdemo.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+降维NMF算法demo，Olivetti人脸数据特征提取
+"""
+import matplotlib.pyplot as plt
+# 加载法包
+from sklearn import decomposition
+# 人脸数据集
+from sklearn.datasets import fetch_olivetti_faces
+from numpy.random import RandomState
+
+
+def plot_gallery(title, images, n_col, n_row):
+    # 设置人脸数据大小
+    image_shape = (64, 64)
+    plt.figure(figsize=(2. * n_col, 2.26 * n_row))
+    plt.suptitle(title, size=16)
+    for i, comp in enumerate(images):
+        plt.subplot(n_row, n_col, i+1)
+        vmax = max(comp.max(), -comp.min())
+        plt.imshow(comp.reshape(image_shape), cmap=plt.cm.gray, interpolation='nearest',
+                   vmin=-vmax, vmax=vmax)
+        plt.xticks(())
+        plt.yticks(())
+    plt.subplots_adjust(0.01, 0.05, 0.99, 0.94, 0.04, 0.)
+
+
+def main():
+    n_row, n_col = 2, 3
+    n_components = n_row * n_col
+    dataset = fetch_olivetti_faces(shuffle=True, random_state=RandomState(0))
+    faces = dataset.data
+    plot_gallery("First centered Olivetti faces", faces[:n_components], n_col, n_row)
+
+    estimators = [
+        ('Eigenfaces - PCA use randomized SVD ', decomposition.PCA(n_components=6, whiten=True)),
+        ('Non-negative components - NMF', decomposition.NMF(n_components=6, init='nndsvda', tol=5e-3))
+    ]
+
+    for name, estimator in estimators:
+        print("Extracting the top %d %s ..." % (n_components, name))
+        print(faces.shape)
+        estimator.fit(faces)
+        components_ = estimator.components_
+        plot_gallery(name, components_[:n_components], n_col, n_row)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
